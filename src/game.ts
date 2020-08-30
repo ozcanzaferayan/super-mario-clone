a13c32291865cb27b534339ae357f196f27b0ad9
Zafer AYAN <ozcanzaferayan@gmail.com>
2020-08-31 01:41:57 +0300

    add

--- src/game.ts
+++ src/game.ts
@@ -3,10 +3,11 @@ import 'phaser';
 export default class Demo extends Phaser.Scene {
     player: any;
     platforms: any;
-    cursors: any;
+    cursors: Phaser.Types.Input.Keyboard.CursorKeys;
     velocity:number = 200;
     frameRate:number = 15;
-    way:string;
+    isLookingLeft: boolean;
+    isCrouched:boolean;
     constructor() {
         super('demo');
     }
@@ -17,8 +18,6 @@ export default class Demo extends Phaser.Scene {
     preload() {
         this.load.image('sky', 'assets/sky.jpg');
         this.load.image('ground', 'assets/ground.png');
-        this.load.image('star', 'assets/star.png');
-        this.load.image('bomb', 'assets/bomb.png');
         this.load.spritesheet('mario', 'assets/mario.png', { frameWidth: 30, frameHeight: 32 });
     }
 
@@ -29,13 +28,21 @@ export default class Demo extends Phaser.Scene {
 
         this.platforms.create(400, 568, 'ground').setScale(1).refreshBody();
 
-        // this.platforms.create(600, 400, 'ground');
-        // this.platforms.create(50, 250, 'ground');
-        // this.platforms.create(750, 220, 'ground');
-
         this.player = this.physics.add.sprite(100, 450, 'mario').setScale(4).refreshBody();
 
         this.player.setCollideWorldBounds(true);
+
+        this.anims.create({
+            key: 'crouchLeft',
+            frames: [{ key: 'mario', frame: 0 }],
+            frameRate: 1
+        });
+
+        this.anims.create({
+            key: 'jumpLeft',
+            frames: [{ key: 'mario', frame: 1 }],
+            frameRate: 1
+        });
 
         this.anims.create({
             key: 'left',
@@ -62,35 +69,70 @@ export default class Demo extends Phaser.Scene {
             frameRate: this.frameRate,
             repeat: -1
         });
+
+        this.anims.create({
+            key: 'jumpRight',
+            frames: [{ key: 'mario', frame: 12 }],
+            frameRate: 1
+        });
+
+        this.anims.create({
+            key: 'crouchRight',
+            frames: [{ key: 'mario', frame: 13 }],
+            frameRate: 1
+        });
+
         this.cursors = this.input.keyboard.createCursorKeys();
         this.physics.add.collider(this.player, this.platforms);
     }
     update() {
+
+        if(this.player.body.velocity.x == 0)
+        {
+            this.player.anims.play(this.isLookingLeft ? 'turnLeft' : 'turnRight');
+        }
+
+        if(this.cursors.down.isDown) {
+            this.player.anims.play(this.isLookingLeft ? 'crouchLeft': 'crouchRight');
+            this.isCrouched = true
+        } else {
+            this.isCrouched = false;
+        }
+
         if (this.cursors.left.isDown)
         {
-            this.way = 'left';
+            this.isLookingLeft = true;
             this.player.setVelocityX(-this.velocity);
-
             this.player.anims.play('left', true);
         }
         else if (this.cursors.right.isDown)
         {
-            this.way = 'right';
+            this.isLookingLeft = false;
             this.player.setVelocityX(this.velocity);
-
             this.player.anims.play('right', true);
-        }
-        else
-        {
-            this.player.setVelocityX(0);
-
-            this.player.anims.play(this.way === 'left' ? 'turnLeft' : 'turnRight');
         }
 
         if (this.cursors.up.isDown && this.player.body.touching.down)
         {
             this.player.setVelocityY(-750);
+            if(!this.isCrouched){
+
+            this.player.anims.play(this.isLookingLeft ? 'jumpLeft' : 'jumpRight');
+            }
         }
+        else if (!this.player.body.touching.down)
+        {
+            if(!this.isCrouched){
+
+            this.player.anims.play(this.isLookingLeft ? 'jumpLeft' : 'jumpRight');
+            }
+        }
+
+
+        if(Math.abs(this.player.body.velocity.x) > 0) {
+            this.player.setVelocityX(this.player.body.velocity.x + (20 * (this.isLookingLeft ? 1 : -1)));
+        }
+
     }
 }
 
